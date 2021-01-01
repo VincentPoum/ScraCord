ed20096d63ff6b57f845b906591711d4e1fec576
pouya mohtacham <pouyajoon@gmail.com>
2021-01-01 12:48:11 +0100

    add users

--- src/common/users/users.ts
+++ src/common/users/users.ts
@@ -1,3 +1,6 @@
 export const users: IUser[] = [
-    { id: 27, name: 'ZouZou' }
+    { id: 27, name: 'ZouZou' },
+    { id: 69, name: 'Pouya' },
+    { id: 83, name: 'Jann0u' },
+    { id: 99, name: 'Vincent' }
 ];
